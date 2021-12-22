84aa6b058849b50dfa77108f2482de50b8e4bcbe
Ivan Tapia Avalos <ivan.tapia@mediaccess.com.mx>
2021-12-22 15:47:42 -0600

    se sube proyecto

---
+++ GenerarContraseñaAleatoria/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerarContraseñaAleatoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdn = new Random();
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%$#@";
+            int longitud = caracteres.Length;
+            char letra;
+            int longitudContrasenia = 10;
+            string contraseniaAleatoria = string.Empty;
+            for (int i = 0; i < longitudContrasenia; i++)
+            {
+                letra = caracteres[rdn.Next(longitud)];
+                contraseniaAleatoria += letra.ToString();
+            }
+            var password = GenerarPassword(10);
+            var pass = generarClaveSHA1("Ivan");
+
+            Console.Write("La contraseña generada es: " + contraseniaAleatoria);
+            Console.WriteLine();
+
+            Console.Write("La pass generada es: " + password);
+            Console.WriteLine();
+            Console.Write("La pass generada es: " + pass);
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+        public static string GenerarPassword(int longitud)
+        {
+            string contraseña = string.Empty;
+            string[] letras = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+            Random EleccionAleatoria = new Random();
+
+            for (int i = 0; i < longitud; i++)
+            {
+                int LetraAleatoria = EleccionAleatoria.Next(0, 100);
+                int NumeroAleatorio = EleccionAleatoria.Next(0, 9);
+
+                if (LetraAleatoria < letras.Length)
+                {
+                    contraseña += letras[LetraAleatoria];
+                }
+                else
+                {
+                    contraseña += NumeroAleatorio.ToString();
+                }
+            }
+            return contraseña;
+        }
+
+        public static string generarClaveSHA1(string cadena)
+        {
+
+            UTF8Encoding enc = new UTF8Encoding();
+            byte[] data = enc.GetBytes(cadena);
+            byte[] result;
+
+            SHA1CryptoServiceProvider sha = new SHA1CryptoServiceProvider();
+
+            result = sha.ComputeHash(data);
+
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+
+                // Convertimos los valores en hexadecimal
+                // cuando tiene una cifra hay que rellenarlo con cero
+                // para que siempre ocupen dos dígitos.
+                if (result[i] < 16)
+                {
+                    sb.Append("0");
+                }
+                sb.Append(result[i].ToString("x"));
+            }
+
+            //Devolvemos la cadena con el hash en mayúsculas para que quede más chuli :)
+            return sb.ToString().ToUpper();
+        }
+    }
+}
+
+   
